35233facaa54d8ff2a765662ee0e92f2ab5432cc
maria aparcida costa pereira andrade <cydaandrade2012@gmail.com>
2024-02-09 12:58:01 -0300

    modificada  js

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -16,31 +16,20 @@ document.getElementById("transaction-form").addEventListener("submit",function(e
     const value=parseFloat(document.getElementById("value-input").value);
     const description=document.getElementById("description-input").value;
     const date=document.getElementById("date-input").value;
-   
 
-    data.transactions.unshift({ 
+      data.transactions.unshift({ 
         value: value, type: type, description:description, date:date
     });
-     
+
     saveData(data);
      e.target.reset();
      myModal.hide();
 
       getTransactions();
-      checkBalance();
-
-      function checkBalance() {
-    const totalBalance = data.transactions.reduce((acc, item) => acc + (item.type === "1" ? item.value : -item.value), 0);
-    console.log(totalBalance)
-    if (totalBalance < 0) {
-        alert("Atenção. Seu saldo após cadastrar essa despesa será negativo, deseja continuar ?");
-    }
-}
 
 
      alert("Lançamento adicionado com sucesso.");
-
-    });
+});
 
       checkLogged();
 
@@ -51,7 +40,7 @@ document.getElementById("transaction-form").addEventListener("submit",function(e
     }
 
          if(!logged){
-        
+
         window.location.href= "index.html";
         return;
 
@@ -67,16 +56,16 @@ document.getElementById("transaction-form").addEventListener("submit",function(e
     function logout() {
     sessionStorage.removeItem("logged");
     localStorage.removeItem("session");
-    
+
     window.location.href="index.html";
-  
+
      }
 
     function getTransactions() {
     const transactions=data.transactions;
     let transactionsHtml=``;
-      
-     
+
+
     if(transactions.length){
      transactions.forEach((item)=>{
         let type="entrada";
@@ -85,7 +74,7 @@ document.getElementById("transaction-form").addEventListener("submit",function(e
             type ="Saida";
 
         }
-   
+
             transactionsHtml+=` 
          <tr>
           <th scope="row">${item.date}</th>
@@ -94,7 +83,7 @@ document.getElementById("transaction-form").addEventListener("submit",function(e
           <td>${item.description}</td>
         </tr>
         ` 
-         
+
      })
     }
     document.getElementById("transactions-list").innerHTML= transactionsHtml;
@@ -103,5 +92,3 @@ document.getElementById("transaction-form").addEventListener("submit",function(e
     function saveData(data){
     localStorage.setItem(data.login, JSON.stringify(data));
 }
-
-
